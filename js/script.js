38e5ff75fb6602f990ee9a267e59a41ec27d7dcb
Vladimir <Luxorchegone@gmail.com>
2020-12-09 18:32:12 +0400

    Дописал механикку открытия/закрытия окна ввода логина и пароля

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict"
 // выпадающее меню для мобильной версии
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+let navMain = document.querySelector('.main-nav'),
+    navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -16,17 +16,17 @@ navToggle.onclick = () => {
 };
 
 // кнопки слайдера отзывов для планшетной/десктопной версии
-let sliderInput1 = document.getElementById('reviews-input-1');
-let sliderInput2 = document.getElementById('reviews-input-2');
-let sliderInput3 = document.getElementById('reviews-input-3');
-let buttonPrev = document.querySelector('.reviews__prev');
-let buttonNext = document.querySelector('.reviews__next');
+let sliderInput1 = document.getElementById('reviews-input-1'),
+    sliderInput2 = document.getElementById('reviews-input-2'),
+    sliderInput3 = document.getElementById('reviews-input-3'),
+    buttonPrev = document.querySelector('.reviews__prev'),
+    buttonNext = document.querySelector('.reviews__next');
 
 buttonPrev.onclick = () => {
     if (sliderInput1.checked) {
         sliderInput1.checked = false;
         sliderInput3.checked = true;
-    } else if (sliderInput2.checked) {  
+    } else if (sliderInput2.checked) {
         sliderInput2.checked = false;
         sliderInput1.checked = true;
     } else if (sliderInput3.checked) {
@@ -39,11 +39,26 @@ buttonNext.onclick = () => {
     if (sliderInput1.checked) {
         sliderInput1.checked = false;
         sliderInput2.checked = true;
-    } else if (sliderInput2.checked) {  
+    } else if (sliderInput2.checked) {
         sliderInput2.checked = false;
         sliderInput3.checked = true;
     } else if (sliderInput3.checked) {
         sliderInput3.checked = false;
         sliderInput1.checked = true;
     }
+};
+
+//механика для открытия/закрытия модального окна входа в личный кабинет
+
+let loginButton = document.querySelector(".user-list__login"),
+    loginForm = document.querySelector(".login-form"),
+    loginformClose = loginForm.querySelector(".login-form__close");
+
+loginButton.onclick = () => {
+    event.preventDefault();
+    loginForm.classList.add("login-form--opened");
+};
+
+loginformClose.onclick = () => {
+    loginForm.classList.remove("login-form--opened");
 };
